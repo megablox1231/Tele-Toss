dcd3d9b3b06065c98aa6a066c39933fb55f95036
Alex Gomez <alxgomez123@gmail.com>
2023-05-03 15:58:04 -0700

    Added Pause Screen

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Logic of pause screen, including pausing, resuming, and quitting.
+/// </summary>
+public class PauseScreen : MonoBehaviour
+{
+    [SerializeField] GameObject pauseUI;
+
+    public static bool gamePaused = false;
+
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gamePaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    void Pause() {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0;
+        gamePaused = true;
+    }
+
+    public void Resume() {
+        StartCoroutine(ResumeHelper());
+    }
+
+    // Ensures mouse input not picked up by PlayerTeleportation
+    IEnumerator ResumeHelper() {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(0.1f);
+        gamePaused = false;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
