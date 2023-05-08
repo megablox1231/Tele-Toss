be788b24d7cea305d6d902382e4298161b4d9e60
Taylor <Taylordvandenberg@gmail.com>
2023-05-08 15:57:21 -0700

    Fixed game over screen and added radioactive box and wall to prefab

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     public void RetryGame(){
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        SceneManager.LoadScene("Level1");
     }
 
     public void QuitGame(){
--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -18,7 +18,7 @@ public class PlayerLife : MonoBehaviour
     }
 
     private void Die(){
-        RestartLevel();
+        SceneManager.LoadScene("GameOver");
     }
 
     private void RestartLevel(){
